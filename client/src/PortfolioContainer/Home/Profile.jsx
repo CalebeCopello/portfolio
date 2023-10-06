7736dbbfcd337356f45a7e59c6f9ddc420818bd0
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-10-05 22:15:20 -0300

    Profile component Done

--- client/src/PortfolioContainer/Home/Profile.jsx
+++ client/src/PortfolioContainer/Home/Profile.jsx
@@ -10,7 +10,7 @@ const Profile = () => {
     React.useEffect(() => {
         const typed = new Typed(el.current, {
         strings: [
-            'Desenvolvedor entusiasmado &#128187;', 
+            'Desenvolvedor entusiasta &#128187;', 
             'Estudante de ADS &#128640;',
             'Aprendiz MERN Stack &#127760;',
             '<i>"Live long and prosper"</i> &#128406;',
@@ -69,10 +69,12 @@ const Profile = () => {
                     </div>
                     <div className="profile-picture">
                         <div className="profile-picture-background">
-
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="profile-footer">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,96L80,90.7C160,85,320,75,480,74.7C640,75,800,85,960,90.7C1120,96,1280,96,1360,96L1440,96L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
             </div>
         </>
     )
