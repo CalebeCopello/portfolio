3dbf53fc59daf53093cb5a1084ebd2531ffad623
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-10-06 22:33:07 -0300

    Header func added

--- client/src/PortfolioContainer/Home/Header/Header.jsx
+++ client/src/PortfolioContainer/Home/Header/Header.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import {TOTALSCREENS, GETSCREENINDEX} from '../../../utils/commonUtils'
 import ScrollService from '../../../utils/ScrollService'
+import {RxHamburgerMenu} from 'react-icons/rx'
+import {FaDev} from 'react-icons/fa'
 import './Header.css'
 
 const Header = () => {
@@ -14,9 +16,45 @@ const Header = () => {
     }
     let currentScreenSubscription = ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen)
 
+    const getHeaderOptions = () => {
+        return(
+            TOTALSCREENS.map((screen, i) => (
+                <div key={screen.screenName} className={getHeaderOptionClass(i)} onClick={() => switchScreen(i, screen)}>
+                    <span>{screen.screenName}</span>
+                </div>
+            ))
+        )
+    }
+
+    const switchScreen = (i, screen) => {
+        let screenComponent = document.getElementById(screen.screenName)
+        if (!screenComponent) return
+        screenComponent.scrollIntoView( {behavior: 'smooth'})
+        setSelectedScreen(i)
+        setShowHeaderOptions(false)
+    }
+
+    const getHeaderOptionClass = (i) => {
+        let classes = 'header-option'
+        if (i < TOTALSCREENS.length -1) classes += 'header-option-separator'
+        if (selectedScreen === i) classes += 'selected-header-option'
+        return
+    }
 
     return (
         <>
+        <div className="header-option" onClick={() => setShowHeaderOptions(!showHeaderOptions)}>
+            <div className="header-parent">
+                <div className="header-hamburger" onClick={() => setShowHeaderOptions(!showHeaderOptions)}></div>
+                <RxHamburgerMenu className='header-hamburger-bars' />
+            </div>
+            <div className="header-logo">
+                <span><FaDev /></span>
+            </div>
+            <div className={(showHeaderOptions) ? 'header-options show-hamburger-options' : 'header-options'}>
+                {getHeaderOptions()}
+            </div>
+        </div>
         </>
     )
 }
--- client/src/PortfolioContainer/Home/Home.jsx
+++ client/src/PortfolioContainer/Home/Home.jsx
@@ -1,3 +1,4 @@
+import Header from "./Header/Header"
 import Profile from "./Profile/Profile"
 import Footer from "./Footer/Footer"
 import './Home.css'
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <>
         <div className="home-container">
+            <Header />
             <Profile />
             <Footer />
         </div>
