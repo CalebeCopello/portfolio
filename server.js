bf44f7758f04f08880bd524e160527cd71d4dc56
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-11-20 16:14:10 -0300

    set for production

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.use(cors())
 
 app.use('/', contactRoute)
 
+if (process.env.NODE_ENV === 'PRODUCTION') {
+    app.use(express.static('client/build'))
+    app.get('*',(req,res)=>(
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    ))
+}
+
 const port = process.env.PORT || 5000
 
 app.listen(port, console.log(`server at port 5000`))
